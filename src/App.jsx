aea5be6613fa15326f8e3916080a53c571edf98d
spurtilo <148346961+spurtilo@users.noreply.github.com>
2025-10-03 14:36:08 +0300

    Initially implement fetching all recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import RecipeList from "./pages/RecipeList";
 import AddRecipe from "./pages/AddRecipe";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
+
+import recipeService from "../services/recipes";
+
 import Header from "../components/Header";
 import RecipeListItem from "../components/RecipeListItem";
-import recipes from "../data/recipes.json";
 
 const RecipeList = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const data = await recipeService.getAll();
+        setRecipes(data);
+      } catch (error) {
+        console.log("ERROR FETCHING RECIPES");
+      }
+    };
+    fetchRecipes();
+  }, []);
+
   return (
     <div className="page-container">
       <Header />
       <div className="recipe-list-container">
-        {recipes.map((recipe, index) => (
+        {recipes?.map((recipe, index) => (
           <RecipeListItem key={index} recipe={recipe} />
         ))}
       </div>
