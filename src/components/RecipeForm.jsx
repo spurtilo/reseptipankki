de930cd7b79c1fc9a77731721588baea4ee193bf
spurtilo <148346961+spurtilo@users.noreply.github.com>
2025-10-06 11:01:41 +0300

    More styling. Display errors below inputs.

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,54 +1,76 @@
-// import { useState } from "react";
-
 const RecipeForm = ({ register, handleSubmit, errors, onSubmit }) => {
+  const urlPattern =
+    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i;
+
   return (
     <div className="recipe-form-container">
       <form className="recipe-form" onSubmit={handleSubmit(onSubmit)}>
+        <h1>SYÖTÄ RESEPTI</h1>
         <div className="form-row">
           <label className="form-label" htmlFor="name">
             Nimi:
           </label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            className="form-field"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <div>This field is required</div>}
+          <div className="form-field-container">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="form-field"
+              {...register("name", { required: true })}
+            />
+            <div className="error">
+              {errors.name && <>Tämä kenttä on pakollinen</>}
+            </div>
+          </div>
         </div>
+
         <div className="form-row">
           <label className="form-label" htmlFor="ingredients">
             Ainekset:
           </label>
-          <input
-            type="text"
-            name="ingredients"
-            className="form-field"
-            {...register("ingredients")}
-          />
+          <div className="form-field-container">
+            <input
+              type="text"
+              name="ingredients"
+              className="form-field"
+              {...register("ingredients", { required: true })}
+            />
+            <div className="error">
+              {errors.ingredients && <>Tämä kenttä on pakollinen</>}
+            </div>
+          </div>
         </div>
         <div className="form-row">
           <label className="form-label" htmlFor="instructions">
             Vaiheet:
           </label>
-          <input
-            type="text"
-            name="instructions"
-            className="form-field"
-            {...register("instructions")}
-          />
+          <div className="form-field-container">
+            <input
+              type="text"
+              name="instructions"
+              className="form-field"
+              {...register("instructions", { required: true })}
+            />
+            <div className="error">
+              {errors.instructions && <>Tämä kenttä on pakollinen</>}
+            </div>
+          </div>
         </div>
         <div className="form-row">
           <label className="form-label" htmlFor="url">
             URL:
           </label>
-          <input
-            type="text"
-            name="url"
-            className="form-field"
-            {...register("url")}
-          />
+          <div className="form-field-container">
+            <input
+              type="text"
+              name="url"
+              className="form-field"
+              {...register("url", { pattern: urlPattern })}
+            />
+            <div className="error">
+              {errors.url && <>Tämä osoite ei ole validi</>}
+            </div>
+          </div>
         </div>
         <button className="form-button" type="submit">
           Submit
